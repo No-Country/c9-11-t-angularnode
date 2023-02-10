99a74bc1a403f84fd5d59883f3b60448d8577d54
YosstinCode <yosstincode@gmail.com>
2023-02-10 17:12:48 -0500

    iniciando con el home page
    - creacion de los botones de categoria
    - agregacion de configuracion para no import react con slint
    - adding react boostrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
-
+import { ButtonSectionFood } from "./components/buttonSectionFood";
+import { Home } from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const App = () => {
   return (
     <>
-      <h1>NO-COUNTRY</h1>
+      {/* <h1>NO-COUNTRY</h1> */}
+      <Home />
+      {/* <ButtonSectionFood></ButtonSectionFood> */}
     </>
-  )
-}
+  );
+};
