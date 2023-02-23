ed733d681ba08ab94bb049209026161dd1989f39
Roman Arenas <roman.ea3232@gmail.com>
2023-02-23 13:01:33 -0300

    add_logic_form_And_register

--- client/src/pages/CreateAccount.jsx
+++ client/src/pages/CreateAccount.jsx
@@ -1,17 +1,38 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import './styles/login.css';
+
+
 
 export const CreateAccount = () => {
   
+  const { register, handleSubmit } = useForm();
+
+  const navigate = useNavigate();
+
+const submit = (data) => {
+  axios
+    .post("https://nc-api-c911t.gpamic.ar/api/v1/register", data)
+    .then((res) => {
+      navigate("/");
+      localStorage.setItem("token", res.data); 
+    })
+    .catch((error) => {
+      if (error) {
+        alert("Credenciales incorrectas");
+      } 
+    });
+};
+
+
   const [show, setShow] = useState(true);
-
   const handleClose = () => setShow(!show);
-
   return (
     <>
-      <form className="login">
+      <form className="login" onSubmit={handleSubmit(submit)}>
         <Modal show={show} onHide={handleClose}>
 
         <Modal.Header closeButton>
@@ -20,27 +41,37 @@ export const CreateAccount = () => {
           <button className="btn btn__enter">Crear cuenta</button>
           <div className="user">
           <label>Nombre y apellido</label>
-            <input type="text" />
+            <input type="text" 
+            {...register("name")}
+            />
           </div>
           <div className="user">
           <label>Email</label>
-            <input type="email" />
+            <input type="email" 
+            {...register("email")}
+            />
           </div>
           <div className="user">
           <label>Numero de celular (opcional)</label>
-            <input type="number" />
+            <input type="number" 
+            {...register("phone")}
+            />
           </div>
           <div className="user">
           <label>Contraseña</label>
-            <input type="password" />
+            <input type="password" 
+            {...register("password")}
+            />
           </div>
           <div className="user">
-          <label>Repetir contraseña</label>
-            <input type="password" />
+          <label>Direccion</label>
+            <input type="text" 
+            {...register("address")}
+            />
           </div>
           </Modal.Body>
           <Modal.Footer className="btn__down">
-            <Button className="btn btn__enter ">
+            <Button className="btn btn__enter " type="submit">
               Ingresar
             </Button>
           </Modal.Footer> 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import './styles/login.css';
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import "./styles/login.css";
 
 export const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+
+  const submit = (data) => {
+    axios
+      .post("https://nc-api-c911t.gpamic.ar/api/v1/login", data)
+      .then((res) => {
+        navigate("/", {
+          replace: true,
+        });
+        localStorage.setItem("token", res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const [show, setShow] = useState(true);
 
@@ -11,28 +29,33 @@ export const Login = () => {
 
   return (
     <>
-      <form className="login">
+      <form className="login" onSubmit={handleSubmit(submit)}>
         <Modal show={show} onHide={handleClose}>
-
-        <Modal.Header closeButton>
-          </Modal.Header>
+          <Modal.Header closeButton></Modal.Header>
           <Modal.Body className="login__container">
-          <button className="btn btn__enter">Acceso Administrador</button>
-          <div className="user">
-          <label>Mail</label>
-            <input type="text" />
-          </div>
-          <div className="user">
-          <label>Contraseña</label>
-            <input type="text" />
-          </div>
+            <button className="btn btn__enter">Acceso Administrador</button>
+            <div className="user">
+              <label>Mail</label>
+              <input
+                type="text"
+                placeholder="Enter email"
+                {...register("email")}
+              />
+            </div>
+            <div className="user">
+              <label>Contraseña</label>
+              <input
+                type="password"
+                {...register("password")}
+                placeholder="password"
+              />
+            </div>
           </Modal.Body>
           <Modal.Footer className="btn__down">
-            <Button className="btn btn__enter ">
+            <Button className="btn btn__enter " type="submit">
               Ingresar
             </Button>
-          </Modal.Footer> 
-        
+          </Modal.Footer>
         </Modal>
       </form>
     </>
