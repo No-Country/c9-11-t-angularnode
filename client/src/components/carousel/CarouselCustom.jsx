0cb065e597ca22b0f6e0c0f408112d991d3c25e5
YosstinCode <yosstincode@gmail.com>
2023-02-23 13:31:22 -0500

    adding roboto font and create section products

--- client/src/components/carousel/CarouselCustom.jsx
+++ client/src/components/carousel/CarouselCustom.jsx
@@ -9,39 +9,43 @@ export default function CarouselCutom() {
   };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
+    <Carousel
+      style={{ width: "100%", height: "150px" }}
+      activeIndex={index}
+      onSelect={handleSelect}
+    >
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400"
+          src="https://via.placeholder.com/360x150"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400"
+          src="https://via.placeholder.com/360x150"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400"
+          src="https://via.placeholder.com/360x150"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h5>Third slide label</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
