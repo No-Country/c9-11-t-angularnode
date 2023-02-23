a57444e5083e19d4d20cd4988432bbd19d36a3c9
thadek <gabipamic@gmail.com>
2023-02-23 16:22:15 -0300

    login changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import { AppRouter } from "./routes/AppRouter";
 import { Footer } from "./ui/Footer/Footer";
 import { Navar } from "./ui/Navar";
+import { ToastContainer } from "react-toastify";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
   return (
     <>
+     
       <Navar />
       <AppRouter />
       <Footer />
+      <ToastContainer />
     </>
   );
 };
