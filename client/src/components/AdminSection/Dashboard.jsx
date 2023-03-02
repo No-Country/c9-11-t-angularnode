b2b1c5fde95236f92a016ba71db094f3a1e0a650
rossmery-garcia <garciarossmery@gmail.com>
2023-03-02 06:55:28 -0300

    Modifications in the Dashboard component

--- client/src/components/AdminSection/Dashboard.jsx
+++ client/src/components/AdminSection/Dashboard.jsx
@@ -1,95 +1,43 @@
 import { useAuth } from "../../hooks/useAuth";
-import { Grid, Typography, Card } from "@mui/material";
 import LocalGroceryStoreRoundedIcon from '@mui/icons-material/LocalGroceryStoreRounded';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 import MenuBookRoundedIcon from '@mui/icons-material/MenuBookRounded';
-import ToggleButton from '@mui/material/ToggleButton';
 import { Link } from 'react-router-dom';
+import './Dashboard.css';
 
 export const Dashboard = () => {
     const { user } = useAuth();
 
-    //Admin Dashboard
     return (
-        <Grid container direction="row" sx={{ background: 'var(--four)' }}>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ background: "linear-gradient(320deg, rgba(255,102,0,0.17690826330532217) 0%, rgba(242,150,11,0.6727065826330532) 100%), url('./src/assets/images/bg2.jpg')", backgroundSize: '150%', backgroundPosition: '50%,5%', height: '30vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                <Typography variant="h2" sx={{ color: '#fff' }}>Dashboard</Typography>
-                <Typography variant="h6" sx={{ color: '#fff' }}>Hola denuevo, {user.name}</Typography>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ background: 'var(--four)', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 4, height: '60vh' }}>
-                <Link to="/admin/products" style={{ textDecoration: 'none' }}><ToggleButton sx={{ border: 'none', padding: 0, textTransform: 'none' }}>
-                    <Card
-                        className="classes.root"
-                        sx={{
-                            width: 200,
-                            height: 200,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            background: 'var(--primary)',
-                            color: '#fff',
-                            borderRadius: 2,
-                            boxShadow: 3,
-                            '&:hover': {
-                                boxShadow: 6,
-                            },
-                        }}
-                    >
-                        <Typography variant="h5"><LocalGroceryStoreRoundedIcon sx={{ fontSize: '2em' }} /></Typography>
-                        <Typography variant="h6">Productos</Typography>
-                    </Card>
-                </ToggleButton></Link>
+        <div className="dashboardContainer">
+            <div className="welcomeContainer">
+                <h2>Dashboard</h2>
+                <h5>{`Hola de nuevo ${user.name}`}</h5>
+            </div>
 
-                <Link to="/admin/categories" style={{ textDecoration: 'none' }}>
-                    <ToggleButton sx={{ border: 'none', padding: 0, textTransform: 'none' }}>
-                        <Card
-                            sx={{
-                                width: 200,
-                                height: 200,
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                background: 'var(--primary)',
-                                color: '#fff',
-                                borderRadius: 2,
-                                boxShadow: 3,
-                                '&:hover': {
-                                    boxShadow: 6,
-                                }
-                            }}
-                        >
-                            <Typography variant="h5"><MenuBookRoundedIcon sx={{ fontSize: '2em' }} /></Typography>
-                            <Typography variant="h6">Categorías</Typography>
-                        </Card>
-                    </ToggleButton>
-                </Link>
+            <div className="cardContainer">
+                <div className="itemCard">
+                    <Link to="/admin/products" style={{ textDecoration: 'none' }} className="link">
+                        <LocalGroceryStoreRoundedIcon sx={{ fontSize: '2em' }} />
+                        <span>Productos</span>
+                    </Link>
+                </div>
 
-                <ToggleButton sx={{ border: 'none', padding: 0, textTransform: 'none' }}><Card
+                <div className="itemCard">
+                    <Link to="/admin/categories" style={{ textDecoration: 'none' }} className="link">
+                        <MenuBookRoundedIcon sx={{ fontSize: '2em' }} />
+                        <span>Categorías</span>
+                    </Link>
+                </div>
 
-                    sx={{
-                        width: 200,
-                        height: 200,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        background: 'var(--primary)',
-                        color: '#fff',
-                        borderRadius: 2,
-                        boxShadow: 3,
-                        '&:hover': {
-                            boxShadow: 6,
+                <div className="itemCard">
+                    <Link to="/admin/users" style={{ textDecoration: 'none' }} className="link">
+                        <PersonRoundedIcon sx={{ fontSize: '2em' }} />
+                        <span>Usuarios</span>
+                    </Link>
+                </div>
+            </div>
 
-                        }
-                    }}
-                >
-                    <Typography variant="h5"><PersonRoundedIcon sx={{ fontSize: '2em' }} /></Typography>
-                    <Typography variant="h6">Usuarios</Typography>
-                </Card>
-                </ToggleButton>
-            </Grid>
-        </Grid>
-    )
+        </div>
+    );
 }
