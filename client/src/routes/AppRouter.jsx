35562623c368f036e123d114881cb267a0ac64d9
rossmery-garcia <garciarossmery@gmail.com>
2023-03-02 02:59:23 -0300

    Added profile view: userProfile component, styles and responsive design

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -12,6 +12,7 @@ import { Products } from "../pages/Products";
 import { CartPage } from "../pages/CartPage";
 import { ProductsPage } from "../pages/ProductsPage";
 import { CartCheckout } from "../components/Cart/CartCheckout";
+import { UserProfile } from "../pages/UserProfile";
 
 
 
@@ -26,12 +27,13 @@ export const AppRouter = () => {
         <Route path="products/:section" element={<Sections />} />
         <Route path="products/:section/:categoryId" element={<Products />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="register" element={<CreateAccount />} /> 
+        <Route path="register" element={<CreateAccount />} />
         <Route path="/admin" element={isAuthenticated ? <AdminPage/> : <Home/>} />
         <Route path="/admin/products" element={isAuthenticated ? <AdminProductsPage/> : <Home/>} />
         <Route path="/admin/categories" element={isAuthenticated ? <AdminCategoriesPage/> : <Home/>} />
         <Route path={'/cart'} element={<CartPage />} />
         <Route path={'/cart/checkout'} element={<CartCheckout />} />
+        <Route path={'/profile'} element={<UserProfile />} />
         <Route path="/*" element={<Home />} />
       </Routes>
     </>
